a3ca1e309543d6dce4a18a1917a56ab7c192ea9a
Deraclassic <chiderauzoigwe2@gmail.com>
2024-02-05 11:10:28 +0100

    commit files

--- src/main/java/Week_1_curriculum/class_activity_2/Main.java
+++ src/main/java/Week_1_curriculum/class_activity_2/Main.java
@@ -1,2 +1,9 @@
-package Week_1_curriculum.class_activity_2;public class Main {
+package Week_1_curriculum.class_activity_2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(OverloadingExample.add(2,3));
+        System.out.println(OverloadingExample.add(2,3,6));
+        System.out.println(OverloadingExample.add(3.4, 45.9));
+    }
 }
--- src/main/java/Week_1_curriculum/class_activity_2/OverloadingExample.java
+++ src/main/java/Week_1_curriculum/class_activity_2/OverloadingExample.java
@@ -1,2 +1,13 @@
-package Week_1_curriculum.class_activity_2;public class OverloadingExample {
+package Week_1_curriculum.class_activity_2;
+
+public class OverloadingExample {
+    static int add(int a, int b){
+        return a + b;
+    }
+    static int add(int a, int b, int c){
+        return a + b +c;
+    }
+    static double add(double a, double b){
+        return a + b;
+    }
 }
