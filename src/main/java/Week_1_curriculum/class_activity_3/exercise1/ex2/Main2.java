00e3799b21b9f6d240d465ee17495b3ef93a197e
Deraclassic <chiderauzoigwe2@gmail.com>
2024-02-06 10:47:25 +0100

    commit files

--- src/main/java/Week_1_curriculum/class_activity_3/exercise1/ex2/Main2.java
+++ src/main/java/Week_1_curriculum/class_activity_3/exercise1/ex2/Main2.java
@@ -1,4 +1,6 @@
-package Week_1_curriculum.class_activity_3.exercise1;
+package Week_1_curriculum.class_activity_3.exercise1.ex2;
+
+import Week_1_curriculum.class_activity_3.exercise1.ex2.Teacher;
 
 public class Main2 {
     public static void main(String[] args) {
